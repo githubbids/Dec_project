b00809f2a51a6ad161299a1d04e240c63164de45
Bidisha Paul <diyasworld@Bidishas-MBP.attlocal.net>
2020-12-01 20:23:16 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from 'react';
+import CityDisplay from './CityDisplay';
 
 function App() {
+  const [city] = useState('London');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React Hooks</h2>
+  <h2>I love {city}</h2>
+  <CityDisplay  cityName={city}/>
     </div>
   );
 }
---
+++ src/CityDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CityDisplay(props){
+    return(
+        <div>
+            <h2>Currently I am staying {props.cityName}</h2>
+        </div>
+    )
+};
